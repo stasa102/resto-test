09736b04550ac1dc806eed6aa5a8a9188f15e444
Stasa Cvijic <stashaa102@gmail.com>
2024-01-03 19:21:34 +0100

    finished gallery and footer section, added scroll-to-top button and finished responsive for min brakepoint

--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ $dropdownBtn.click(function () {
   $dropdownItems.toggle();
   console.log();
 });
+
+// scroll to top
+//scrollTop returns vertical position of element
+$(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $("#scroll-to-top").fadeIn();
+    } else {
+      $("#scroll-to-top").fadeOut();
+    }
+  });
+  $("#scroll-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500);
+  });
+});
